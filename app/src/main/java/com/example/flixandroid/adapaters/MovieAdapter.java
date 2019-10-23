2ddc14afc6a3e01633f68c25b8db0e8bd77a3979
michelle yu <y.michelle03@gmail.com>
2019-10-23 11:17:01 -0400

    problem with accessing movie variable from inner class

--- app/src/main/java/com/example/flixandroid/adapaters/MovieAdapter.java
+++ app/src/main/java/com/example/flixandroid/adapaters/MovieAdapter.java
@@ -1,20 +1,26 @@
 package com.example.flixandroid.adapaters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixandroid.DetailActivity;
 import com.example.flixandroid.R;
 import com.example.flixandroid.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -22,7 +28,7 @@ public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.ViewHolder>
 
     Context context;
     List<Movie> movies;
-
+    Movie movie;
     public MovieAdapter (Context context, List<Movie> movies){
         this.context = context;
         this.movies = movies;
@@ -42,7 +48,7 @@ public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.ViewHolder>
     public void onBindViewHolder (@NonNull ViewHolder holder, int position) {
         Log.d("MovieAdapter", "OnBindViewHolder " + position);
         // get the movie at the passed in position
-        Movie movie = movies.get(position);
+        final Movie movie = movies.get(position);
         //Bind the movie data into the view holder
         holder.bind(movie);
     }
@@ -54,6 +60,7 @@ public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.ViewHolder>
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -63,13 +70,15 @@ public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.ViewHolder>
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
-            //if photo is in landscape, then imageUrl =  backdrop image.
+
+            //if phone is in landscape, then imageUrl =  backdrop image.
             if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
                 imageUrl = movie.getBackdropPath();
             }
@@ -78,6 +87,22 @@ public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.ViewHolder>
                 imageUrl = movie.getPosterPath();
             }
             Glide.with(context).load(imageUrl).into(ivPoster);
+
+
+//            1. register click listener on the whole row.
+            container.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    //            2. navigate to a new activity on tap.
+                    Toast.makeText (context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+
+                    Intent i = new Intent(context, DetailActivity.class);
+//                    i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+
+                }
+            } );
         }
     }
 }
